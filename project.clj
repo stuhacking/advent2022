a4eca731e16c86b80dd9df2b8713872fe3d58f99
Stuart Hacking <stuhacking@gmail.com>
2022-12-05 15:27:26 +0000

    Day 5 Solution

---
+++ project.clj
@@ -0,0 +1,8 @@
+(defproject advent2022 "0.1.0-SNAPSHOT"
+  :description "Literate Clojure Advent of Code 2022"
+  :url "http://github.com/stuhacking/advent2022"
+  :license {:name "MIT"
+            :url "https://mit-license.org"}
+  :dependencies [[org.clojure/clojure "1.10.3"]]
+  :repl-options {:init-ns advent2022.core}
+  :source-paths ["src"])
